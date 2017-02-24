3ef6b6ba8aaa826d9baa5e6d4fcbbe41bd5c4652
Paul Caskey <pcaskey@internet2.edu>
2017-02-24 17:56:06 -0600

    change TIER beacon to JSON/REST

--- files/bin/sendtierbeacon.sh
+++ files/bin/sendtierbeacon.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 LOGHOST="collector.testbed.tier.internet2.edu"
-LOGPORT="5000"
+LOGPORT="5001"
 if [ -s /opt/tier/env.bash ]; then
   . /opt/tier/env.bash
 fi
-LOGTEXT="TIERBEACON/TIER/1.0#IM=$IMAGENAME#PV=$VERSION#TR=$TIERVERSION#MT=$MAINTAINER#"
+
+#below for syslog, F-TICKS style
+#LOGTEXT="TIERBEACON/TIER/1.0#IM=$IMAGENAME#PV=$VERSION#TR=$TIERVERSION#MT=$MAINTAINER#"
+
+#below for JSON/REST style
+LOGTEXT="{ \"msgType\" : \"TIERBEACON\", \"msgName\" : \"TIER\", \"msgVersion\" : \"1.0\", \"tbProduct\" : \"$IMAGENAME\", \"tbProductVersion\" : \"$VERSION\", \"tbTIERRelease\" : \"$TIERVERSION\", \"tbMaintainer\" : \"$MAINTAINER\" }"
+
+
 if [ -z "$TIER_BEACON_OPT_OUT" ]; then
-  `logger -n $LOGHOST -P $LOGPORT -t TIERBEACON $LOGTEXT`
+  #send JSON
+  echo $LOGTEXT > msgjson.txt
+  curl -s -XPOST "${LOGHOST}:${LOGPORT}/" -H 'Content-Type: application/json' -T msgjson.txt 1>/dev/null
+  rm -f msgjson.txt
+  
+  #below is for syslog, F-TICKS style
+  #`logger -n $LOGHOST -P $LOGPORT -t TIERBEACON $LOGTEXT`
+
   echo `date`"; TIER beacon sent."
 fi
