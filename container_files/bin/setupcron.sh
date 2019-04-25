366de0ae3764a19ef65262092550d8fc83156dbb
Paul Caskey <pcaskey@internet2.edu>
2019-04-25 09:50:04 -0500

    Update setupcron.sh

--- container_files/bin/setupcron.sh
+++ container_files/bin/setupcron.sh
@@ -8,7 +8,7 @@ CRONFILE=/opt/tier/tier-cron
 #build crontab file with random start time between midnight and 3:59am
 echo "#send daily beacon to TIER Central" > ${CRONFILE}
 echo $(expr $RANDOM % 59) $(expr $RANDOM % 3) "* * * /usr/bin/sendtierbeacon.sh >> /var/log/cron.log 2>&1" >> ${CRONFILE}
-echo "#rotate IdP data sealer key" > ${CRONFILE}
+echo "#rotate IdP data sealer key" >> ${CRONFILE}
 echo "0 1 * * * /opt/shibboleth-idp/bin/rotateSealerKey.sh >> /var/log/cron.log 2>&1" >> ${CRONFILE}
 chmod 644 ${CRONFILE}
 
