bacf9d90eba7a88ccb00684439b188fb01859619
Jan Pavlicek <469355@mail.muni.cz>
2024-04-16 12:33:28 +0200

    install oidc plugins

---
+++ test-compose/idp/install_plugins.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect
+
+set timeout -1
+
+set plugins [list "net.shibboleth.oidc.common" \
+              "net.shibboleth.idp.plugin.oidc.config" \
+              "net.shibboleth.idp.plugin.oidc.op"]
+
+foreach plugin $plugins {
+    spawn bash -c "/opt/shibboleth-idp/bin/plugin.sh -I $plugin"
+    expect -re {\[yN\]} {
+        exp_send "y\r"
+        expect eof
+    }
+}
